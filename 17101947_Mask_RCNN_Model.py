11bdfbdba14d153d9f6b2cea7965eec5d725306c
PrrrStar <jmeef0802@gmail.com>
2020-06-25 23:24:03 +0900

    edit model

--- 17101947_Mask_RCNN_Model.py
+++ 17101947_Mask_RCNN_Model.py
@@ -25,13 +25,10 @@ class loadImage(object):
     def __init__(self, root, transforms):
         self.root = root
         self.transforms = transforms
-        # load all image files, sorting them to
-        # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
         self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
 
     def __getitem__(self, idx):
-        # load images ad masks
         img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
         mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
         img = Image.open(img_path).convert("RGB")
@@ -42,11 +39,8 @@ class loadImage(object):
         obj_ids = np.unique(mask)
         obj_ids = obj_ids[1:]
 
-        # split the color-encoded mask into a set
-        # of binary masks
         masks = mask == obj_ids[:, None, None]
 
-        # get bounding box coordinates for each mask
         num_objs = len(obj_ids)
         boxes = []
         for i in range(num_objs):
@@ -57,15 +51,12 @@ class loadImage(object):
             ymax = np.max(pos[0])
             boxes.append([xmin, ymin, xmax, ymax])
 
-        # convert everything into a torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
         labels = torch.ones((num_objs,), dtype=torch.int64)
         masks = torch.as_tensor(masks, dtype=torch.uint8)
 
         image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
         target = {}
@@ -216,7 +207,7 @@ def main():
         collate_fn=utils.collate_fn)
 
     model = get_model_instance_segmentation(num_classes)
-    model = nn.DataParallel(model)
+    #model = nn.DataParallel(model)
     model = model.to(device)
     
     params = [p for p in model.parameters() if p.requires_grad]
@@ -227,7 +218,7 @@ def main():
                                                    step_size=3,
                                                    gamma=0.1)
     
-    num_epochs = 3
+    num_epochs = 10
 
     for epoch in range(num_epochs):
         train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
